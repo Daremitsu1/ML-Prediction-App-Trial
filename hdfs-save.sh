d3c074cfa573167c3a4693b013f0f7dfc19f702f
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2022-12-17 21:52:12 +0530

    Save files in hdfs

---
+++ hdfs-save.sh
@@ -0,0 +1,3 @@
+hadoop fs -copyFromLocal regression_predictions.csv /path/to/hdfs/directory
+hadoop fs -copyFromLocal classification_predictions.csv /path/to/hdfs/directory
+hadoop fs -copyFromLocal clustering_predictions.csv /path/to/hdfs/directory
